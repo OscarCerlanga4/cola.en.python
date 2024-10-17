e23d6221373f289c68e4202b685f63c743b49b33
OscarCerlanga4 <oscarcerlanga@iesrioarba.es>
2024-10-17 12:57:55 +0200

    .

--- cola.py
+++ cola.py
@@ -1,45 +1,86 @@
 #! C:\Users\alumno24\AppData\Local\Microsoft\WindowsApps\python.exe
 
 
+
 class cola:
-    
+
     __data = []
-    __size = None
-    
+    __maxSize = None  
+
     def __init__(self, size):
-        self.__size = size
-        
-    def h(self) -> int:
+        self.__maxSize = size
+
+    def len(self) -> int: 
         return len(self.__data)
-        
-    def __str__ (self) -> str:
-            answ = "<"
-            answ += f"{self.h()} de {self.__size}, {self.__data}"
-           # if self.esVacia(): answ += "VACIA"
-          #  if self.esLlena(): answ += "LLENA"
-            answ += ">"
-            return answ
-            
-    def esVacia(self) -> bool: 
-        pass 
-    def esLLena(self) -> bool: 
-       return False
-            
-    def enqueue(self ,something):
-         if not self.esLLena():
-            self.__data.append(something)
+
+    def __str__(self) -> str:
+        answ = "<"
+        answ += f"{self.len()} de {self.__maxSize}, {self.__data}"
+        if self.esVacia(): answ += "VACIA"
+        if self.esLlena(): answ += "LLENA"
+        answ += ">"
+        return answ
+
+    def esVacia(self) -> bool:
+        return len(self.__data) == 0
+
+    def esLlena(self) -> bool:
+        return len(self.__data) == self.__maxSize
     
-   #  def dqueue () -> object:
-    #     pass
+    def enqueue(self, something):
+        if not self.esLlena():
+            self.__data.append(something)
+        else:
+            raise OverflowError (f'queue: ya estoy llena')
+
+    def dqueue(self) -> object:
+        if len(self.__data) == 0:
+            raise ValueError (f'queue: esta vacia')
+        else:
+            item = self.__data[0]
+            self.__data = self.__data[1:]
+            return item
 
 if __name__ == "__main__":
-    
+
     c = cola(4)
     print(c)
-    
-    c.enqueue (11)
-    print (c)
-    c.enqueue (22)
-    print (c)
-    
-    
+
+    try:
+        c.enqueue(11)
+        print(c)
+
+        c.enqueue(22)
+        print(c)    
+
+        c.enqueue(33)
+        print(c)
+
+        c.enqueue(44)
+        print(c)
+
+        c.enqueue(55)
+        print(c)
+
+    except OverflowError:
+        print("no entro, estaba llena")
+
+    print(c)
+
+    item = c.dqueue()
+    print(item, c)
+
+    item = c.dqueue()
+    print(item, c)
+
+    item = c.dqueue()
+    print(item, c)
+
+    item = c.dqueue()
+    print(item, c)
+
+    try:
+        item = c.dqueue()
+        print(item, c)
+    except ValueError:
+        print("estaba vacia")
